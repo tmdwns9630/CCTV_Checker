0a1f112026a8c339e3d7661b230836cd559c6bc9
CSJ <tmdwns9630@gmail.cm>
2022-10-13 10:06:56 +0900

    노드모듈/prisma/indexd.ts를 수정하였다. get cCTV_DATA를 임의로 첫글자 대문자로 바꿨었는데 이게 문제인듯

--- pages/api/location/all.ts
+++ pages/api/location/all.ts
@@ -24,19 +24,17 @@ export default async function handler(
   }
   try {
     //console.log(req.query);
-    const cctvS = await client.CCTV_DATA.findMany();
+    const cctvS = await client.cCTV_DATA.findMany();
     console.log("======================");
     //console.log(cctvS.filter((ele, idx) => ele.num < 5));
     const ResCCTVonFive = cctvS.filter((ele, idx) => ele.num <= 5);
     //console.log(ResCCTVonFive);
     //num 5 이하의 cctv 정보만 로드하였음.
-    res
-      .status(200)
-      .json({
-        ok: true,
-        message: "cctv데이터를 로드했습니다.",
-        cctvS: ResCCTVonFive,
-      });
+    res.status(200).json({
+      ok: true,
+      message: "cctv데이터를 로드했습니다.",
+      cctvS: ResCCTVonFive,
+    });
   } catch (err) {
     res.status(200).json({
       ok: false,
