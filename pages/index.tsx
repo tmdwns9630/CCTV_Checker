af32d706b8916174c74d1c82bfaa5714a152ef55
CSJ <tmdwns9630@gmail.cm>
2022-10-17 09:38:31 +0900

    CCTV 위치까지 일단락

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,26 +6,10 @@ import Map from "../components/Map";
 // import Image from "next/image";
 // import styles from "../styles/Home.module.css";
 import { CCTV_DATA } from "@prisma/client";
-import useCurrentPosition from "../components/useCurrentPosition";
 
 const Home: NextPage = () => {
   const [cctvdata, setCctvdata] = useState<CCTV_DATA[]>([]);
 
-  const geolocationOptions = {
-    enableHighAccuracy: true,
-    timeout: 1000 * 60 * 1, // 1 min (1000 ms * 60 sec * 1 minute = 60 000ms)
-    maximumAge: 1000 * 3600 * 24, // 24 hour
-  };
-
-  // const showLatLong = () => {
-  //   const { location: currentLocation, error: currentError } =
-  //     useCurrentPosition(geolocationOptions);
-  //   useEffect(() => {
-  //     console.log("showLatLong 컴포넌트 렌더링");
-  //   }, []);
-
-  //   return <div>{(location.latitude, location.longitude)}</div>;
-  // };
   useEffect(() => {
     fetch("/api/location/all")
       .then((res) => res.json())
@@ -39,13 +23,13 @@ const Home: NextPage = () => {
   return (
     <Layout title="CCTV MAP">
       <div>
-        <div>홈페이지</div>
+        <div>홈페이지_</div>
         <Map latitude={36.7961} longitude={127.0697}></Map>
 
         {cctvdata.map((cctv, idx) => (
           <div key={idx} className="flex space-x-3">
-            {/* <div>CCTV 번호 : {cctv.num}</div>
-            <div>CCTV 위치 : {cctv.detail_address}</div> */}
+            <div>CCTV 번호 : {cctv.num}</div>
+            <div>CCTV 위치 : {cctv.detail_address}</div>
           </div>
         ))}
       </div>
